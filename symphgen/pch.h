0ff0da788428772b41f153cd76d0057aba5c1c66
Hedonist <originalhedonist@gmail.com>
2020-03-29 14:31:54 +0100

    add symphgen

---
+++ symphgen/pch.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef PCH_H
+#define PCH_H
+
+#define _USE_MATH_DEFINES
+#define exprtk_disable_caseinsensitivity
+
+#include <fstream>
+#include <iostream>
+#include "../wavelib/json.hpp"
+#include "../wavelib/exprtk.hpp"
+#include "../wavelib/json.hpp"
+
+#endif

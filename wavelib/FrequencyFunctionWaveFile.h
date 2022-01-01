161c01a256d3bdd269bfe9c6440363fc7670ff6c
Hedonist <originalhedonist@gmail.com>
2022-01-01 18:20:48 +0000

    updates

--- wavelib/FrequencyFunctionWaveFile.h
+++ wavelib/FrequencyFunctionWaveFile.h
@@ -35,22 +35,33 @@ public:
 class normalizefunction : public exprtk::ivararg_function<double>
 {
 public:
-    static const int NORMALIZE_MEM_SIZE = 44100;
+    static const int NORMALIZE_MEM_SIZE = 44100*10; //10 seconds
 
-    double max[NORMALIZE_MEM_SIZE]; // a second
+    double mem[NORMALIZE_MEM_SIZE]; // a second
     normalizefunction()
     {
-        memset(max, 0, sizeof(double) * NORMALIZE_MEM_SIZE);
+        memset(mem, 0, sizeof(double) * NORMALIZE_MEM_SIZE);
     }
 
     virtual inline double operator() (const std::vector<double>& params)
     {
         int n = (long)params[0];
-        double val = params[1];
-        int nBase = n % NORMALIZE_MEM_SIZE;
-        max[nBase] = abs(val);
-        double maxVal = *std::max_element(max, max + NORMALIZE_MEM_SIZE);        
-        return maxVal == 0 ? -0.3 : val / maxVal;
+        int scan = (long)params[1];
+        if(scan <= 0 || scan > NORMALIZE_MEM_SIZE)
+        {
+            throw std::runtime_error("Scan size must be between 1 and NORMALIZE_MEM_SIZE.");
+        }
+        double val = params[2];
+        int nBase = n % scan;
+        mem[nBase] = val;
+        double maxVal = *std::max_element(mem, mem + scan);        
+        double minVal = *std::min_element(mem, mem + scan);
+        if(maxVal > minVal && val >= minVal && maxVal >= val)
+        {
+            double proportion = (val - minVal) / (maxVal - minVal);
+            return (proportion * 2) - 1;
+        }
+        else return val;
     }
 
     virtual ~normalizefunction() {}

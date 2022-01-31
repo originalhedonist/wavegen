0f805c29f57fe34c4178db5555d54ac07c8959dc
Hedonist <originalhedonist@gmail.com>
2022-01-31 18:06:52 +0000

    Committing all files prior to making Freddie

--- makemp3.sh
+++ makemp3.sh
@@ -1,5 +1,16 @@
-if [[ $(git add --all --dry-run) ]]; then
-    echo "There are changes to be committed"
-else
-    echo "There are no changes to be committed"
+# check for outstanding commits instead of just committing them?
+# if [[ $(git add --all --dry-run) ]]; then
+#    echo "There are changes to be committed - please commit them first"
+# else    
+# ...
+# fi
+
+json=$1.json
+if [ ! -f $json ]; then
+    echo $json" does not exist."
+    exit 1
 fi
+
+commitmessage="Committing all files prior to making "$1
+echo $commitmessage
+git add --all && git commit -m "$commitmessage"
